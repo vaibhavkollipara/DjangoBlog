e4d38d11fa57c614e3805fbe7e5d5d129dfe761a
Vaibhav Kollipara <vaibhav.kollipara@gmail.com>
2017-03-31 16:29:38 -0400

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,8 @@ app_name="main"
 
 urlpatterns = [
     url(r'^$', views.index , name="index"),
-    url(r'details/(?P<slug>[a-zA-Z0-9-]+)/$', views.detail, name="detail"),
-    url(r'newpost/$', views.new_post , name="newpost"),
-    url(r'update/(?P<slug>[a-zA-Z0-9-]+)/$', views.update_post, name="update"),
-    url(r'delete/(?P<slug>[a-zA-Z0-9-]+)/$',views.delete_post , name="delete")
+    url(r'^details/(?P<slug>[a-zA-Z0-9-]+)/$', views.detail, name="detail"),
+    url(r'^newpost/$', views.new_post , name="newpost"),
+    url(r'^update/(?P<slug>[a-zA-Z0-9-]+)/$', views.update_post, name="update"),
+    url(r'^delete/(?P<slug>[a-zA-Z0-9-]+)/$',views.delete_post , name="delete")
 ]
